2498888333cdb06a8f3dd3b9dc06678e52359fc0
Vasua Pupkin <you@gmail.com>
2021-07-29 23:45:38 +0600

    adding

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute ('src');
+    if(mySrc==='images/Screenshot.jpg'){
+      myImage.setAttribute ('src','images/Screenshot.jpg')
+    } else {
+        myImage.setAttribute('src','images/copy.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+      } else {
+        localStorage.setItem('name', myName);
+    myHeading.textContent='Dog is cool,' + myName;
+    }
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+}else{
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent= 'Dog is cool,' + storedName;
+}
+myButton.onclick = function(){
+    setUserName();
+}
